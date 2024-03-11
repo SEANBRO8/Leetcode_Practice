431cfc6ba23daae40c3f51aff14fea014ef136c6
GEZI Pro <mansondeng@outlook.com>
2024-03-11 13:44:08 -0700

    889-PostOrder-PreOrder-update

--- BinaryTree/Construct Binary Tree/889.Construct-Binary-Tree-from-PreOrder-And-PostOrder.java
+++ BinaryTree/Construct Binary Tree/889.Construct-Binary-Tree-from-PreOrder-And-PostOrder.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+    // Solution one construct from post to pre
     Map<Integer, Integer> map = new HashMap<>();
 
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
@@ -26,5 +26,27 @@ class Solution {
         root.left = build(preorder, postorder, preStart + 1, leftSize + preStart, postStart, leftRootValIndex);
         root.right = build(preorder, postorder, leftSize + preStart + 1, preEnd, leftRootValIndex + 1, postEnd -1);
         return root;
+    }
+    // Solution two post to pre
+
+    public TreeNode build(int[] preorder, int[] postorder, int preStart, int preEnd, int postStart, int postEnd){
+        // pre:  N L R
+        // post: L R N  
+        if(postStart > postEnd) return null;
+        if(postStart == postEnd) return new TreeNode(postorder[postEnd]);
+        
+        int rootValue = postorder[postEnd];
+        int rightValue = postorder[postEnd - 1];
+
+        int rightRootIndex = map.get(rightValue);
+
+        int leftSize = rightRootIndex - preStart - 1;
+
+        TreeNode root = new TreeNode(rootValue);        
+        root.left = build(preorder, postorder, preStart + 1, preStart + leftSize, postStart, postStart + leftSize - 1 );
+        root.right = build(preorder, postorder, preStart + leftSize + 1, preEnd, postStart + leftSize, postEnd - 1);
+
+        return root;
+
     }
 }
