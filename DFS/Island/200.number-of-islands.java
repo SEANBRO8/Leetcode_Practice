639e94c309f9fdd6c3fbba34e79364b72ddb0a71
GEZI Pro <mansondeng@outlook.com>
2024-03-13 14:26:46 -0700

    200

--- DFS/Island/200.number-of-islands.java
+++ DFS/Island/200.number-of-islands.java
@@ -6,8 +6,33 @@
 
 // @lc code=start
 class Solution {
+    int[][] dirs = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
+   
     public int numIslands(char[][] grid) {
-        
+        int m = grid.length, n = grid[0].length;
+        int ans = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] =='1'){
+                    ans++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public void dfs(char[][] grid, int i, int j){
+        int m = grid.length, n = grid[0].length;
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+
+        for(int p = 0; p < 4; p++){
+            dfs(grid, i + dirs[p][0], j + dirs[p][1]);
+        }
     }
 }
 // @lc code=end
