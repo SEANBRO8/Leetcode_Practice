26788eb618fcd4138ba7004609c101dc5802ba17
GEZI Pro <mansondeng@outlook.com>
2024-03-14 15:39:31 -0700

    LCA 1676

---
+++ 1676.lowest-common-ancestor-of-a-binary-tree-iv.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=1676 lang=java
+ *
+ * [1676] Lowest Common Ancestor of a Binary Tree IV
+ */
+
+// @lc code=start
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        Set<TreeNode> set = new HashSet<>();
+
+        for(TreeNode node : nodes){
+            set.add(node);
+        }
+        return search(root, set);
+    }
+
+    public TreeNode search(TreeNode root, Set<TreeNode> set){
+        if(root == null) return null;
+
+        if(set.contains(root)) return root;
+
+        TreeNode left = search(root.left, set);
+        TreeNode right = search(root.right, set);
+
+        if(left != null && right != null) return root;
+
+        if(left != null) return left;
+        else return right;
+
+
+    }
+}
+// @lc code=end
+
